17a247f2515c27635ae2bd87257a50f307b40056
aymeric.sabatier <aymeric.sabatier@absolut-system.com>
2025-01-23 15:26:50 +0100

    [FIX] Changement css

--- src/page/projectPage/projectPage.tsx
+++ src/page/projectPage/projectPage.tsx
@@ -11,7 +11,7 @@ return(
         <p>Voici quelques projets que j'ai réalisés</p>
         <div className="project_detail">
             <Project title="Site aux enchères" technologies={['PHP', 'SQL', 'GIT']} description="Ce projet nous a permis de réaliser un site aux enchères. Accompagné d'une base de données en SQL"
-                img_description={['enchères.png']} role="Developpeur Bakc end, Réalisation des fonctionnalités liant l'interface et la base de donnée"/>
+                img_description={['enchères.png']} role="Developpeur Backend, Réalisation des fonctionnalités liant l'interface et la base de donnée"/>
             <Project title="Black Hole Bot" technologies={['Python', 'GIT']} description="A l'occasion d'une GameJam organisée par l'IUT 2 Informatique de Grenoble, nous avons développé en groupe de 4, un jeu sur le thème d'un robot et de la gravité. Dans ce jeu, il va falloir survivre 60 secondes sous différentes gravités afin de récupérer des pièces pour réparer son vaisseau. Ce jeu a été réalisé à l'aide de la librairie python pygame."
                 img_description={['blackholebot-1.png']} role="Ecriture du story board et developpeur Full stack. Aide à la réalisation du design et des fonctionnalités, particulièremet la gravité"/>
             <Project title="Tour des héros" technologies={['Angular', 'Firebase', 'GIT']} description="Ce projet nous a permis découvrir le farmework angular. Nous devions créer un projet de héros. Celui ci nous permettait de créer des héros, en leur assignant des compétences mais aussi des armes. Ceux ci pouvait se battre entre eux. Les dégats infligés était calculés en alétaoire pondéré en fonction de la défense du héros recevant le coup et de l'attaque du héros attaquant."
