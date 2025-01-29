c525895490bfe70af3fedd22577752b5c3b1ddfc
aymeric.sabatier <aymeric.sabatier@absolut-system.com>
2025-01-29 11:58:54 +0100

    [FIX] Modif lien

--- src/components/linkMe/linkMe.tsx
+++ src/components/linkMe/linkMe.tsx
@@ -17,7 +17,7 @@ const LinkMe: React.FC = () => {
             label: 'Linkedin',
             icon: 'pi pi-linkedin',
             command: () => {
-                window.open('www.linkedin.com/in/aymeric-sabatier-916613279', '_blank');
+                window.open('https://www.linkedin.com/in/aymeric-sabatier-916613279', '_blank');
             }
         },
         {
